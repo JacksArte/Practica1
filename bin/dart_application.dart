7f86a838158ed21e8c936268850d740e86e1664b
Jaqueline <jaqui2000arte@gmail.com>
2021-01-22 14:13:00 -0600

    Primer Commit

---
+++ bin/dart_application.dart
@@ -0,0 +1,18 @@
+import 'persona.dart';
+import 'triangulo.dart';
+
+void main(List<String> arguments) {
+  /*
+  Triangulo triangulo = new Triangulo();
+
+  triangulo.base = 7.5;
+  triangulo.altura = 23.5;
+
+  print('El area del triangulo es: ${triangulo.area}');
+  */
+
+  Persona p = Persona();
+  p.nombre = 'Jaqueline';
+
+  print('El nombre es: ${p.nombrePersona}');
+}
